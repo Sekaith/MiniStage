47bbcba6c8975aa185b9b3093478d8f4e42ef5e4
Sekaith <73703314+Sekaith@users.noreply.github.com>
2023-02-24 16:38:11 +0100

    rqtModifMS refacto

--- APPLI/requete/rqtModifMS.php
+++ APPLI/requete/rqtModifMS.php
@@ -4,11 +4,18 @@
 function get_MS(){
     global $mysqli;
 	
-    $rqt = 'SELECT m.id, tf.nom as typeformation, f.nom as formation, f.id as idformation, civilite, nomProf, date as dateUS, DATE_FORMAT(date, "%d-%m-%Y") AS dateFR, 
-	hdebut, hfin, nbplace, nbplacereste, lieu
-	from t_ministage as m inner join t_formation as f on m.idformation=f.id
-	inner join t_typeformation as tf on f.idtype=tf.id
-	where m.id= '.$_GET['id'].'';
+//    $rqt = 'SELECT m.id, tf.nom as typeformation, f.nom as formation, f.id as idformation, civilite, nomProf, date as dateUS, DATE_FORMAT(date, "%d-%m-%Y") AS dateFR,
+//	hdebut, hfin, nbplace, nbplacereste, lieu
+//	from t_ministage as m inner join t_formation as f on m.idformation=f.id
+//	inner join t_typeformation as tf on f.idtype=tf.id
+//	where m.id= '.$_GET['id'].'';
+
+    $rqt = 'SELECT m.idministage as id, tf.nom_typeformation  as typeformation, f.nom_formation as formation, f.idformation as idformation, p.civilite, p.nom_prof, date as dateUS, DATE_FORMAT(date, "%d-%m-%Y") AS dateFR, 
+	hdebut, hfin, nbplace, lieu
+	from t_ministage as m inner join t_formation as f on m.idformation=f.idformation
+	inner join t_typeformation as tf on f.idtypeform=tf.idtypeform
+    inner join t_professeur as p on p.idProf = m.idProf
+	where m.idministage= '.$_GET['id'].'';
     $tab= mysqli_query($mysqli,$rqt) or exit(mysqli_error());
 	$MS = $tab -> fetch_assoc();
 	
@@ -21,7 +28,7 @@ function updateMS(){
 	
     $rqt = 'UPDATE t_ministage SET civilite="'.$_POST['civilite'].'", nomProf="'.$_POST['nomprof'].'", 
 	date= STR_TO_DATE("'.$_POST['date'].'", "%d-%m-%Y"), hdebut="'.$_POST['heure1'].'", hfin="'.$_POST['heure2'].'", 
-	nbplace='.$_POST['place'].', nbplacereste='.$_POST['placereste'].', lieu="'.$_POST['lieu'].'"
+	nbplace='.$_POST['place'].', lieu="'.$_POST['lieu'].'"
 	where id= '.$_POST['id'].'';
     mysqli_query($mysqli,$rqt) or exit(mysqli_error());
 }
