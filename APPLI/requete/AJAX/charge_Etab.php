dc049ed9647263a86bdbe29846450aab2910769c
Sekaith <73703314+Sekaith@users.noreply.github.com>
2023-03-15 15:09:20 +0100

    Modif rqt chargement établissement quand rech par formation

--- APPLI/requete/AJAX/charge_Etab.php
+++ APPLI/requete/AJAX/charge_Etab.php
@@ -10,9 +10,12 @@
 //	inner join t_typeetab as t on u.idtype=t.id
 //	where idformation=".$_POST['formation']." order by nometab asc";
 
-    $rqt = "SELECT u.idcompte as id, t.nom_typeetab as nom, e.nometab, e.ville from t_ministage as m inner join t_utilisateur as u on m.idOffrant=u.idcompte
-    inner join t_etablissement as e on u.idetab=e.idetab inner join t_typeetab as t on e.idtypeetab=t.idtypeetab
-	where m.idformation=".$_POST['formation']." order by e.nometab asc";
+    $rqt = "SELECT c.idcompte as id, t.nom_typeetab as nom, e.nometab, e.ville from t_ministage as m 
+    inner join t_compte as c on m.idOffrant=c.idcompte
+    inner join t_etablissement as e on c.idetab=e.idetab 
+    inner join t_formation_compte as fc on fc.idcompte = c.idcompte
+    inner join t_typeetab as t on e.idtypeetab=t.idtypeetab
+	where fc.idformation=".$_POST['formation']." order by e.nometab asc";
 	$Formation = $mysqli->query($rqt);
 			
 			
